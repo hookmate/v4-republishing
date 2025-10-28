68ab27f32a0b8c039d9799c7e2f42b707ef59303
Akshat Mittal <itsreallyakshat@gmail.com>
2025-10-28 18:51:51 +0530

    Update script

--- src/index.ts
+++ src/index.ts
@@ -1,52 +1,63 @@
 import { readFile, writeFile, mkdir, rm } from "node:fs/promises";
+
+import { PackumentVersion } from "@npm/types";
+import { gt } from "semver";
 import { $ } from "zx";
 import ky from "ky";
-
 import { extract } from "tar";
 
 async function main() {
-  await rm("packages", { recursive: true, force: true });
-  await mkdir("packages/source", { recursive: true });
-  await mkdir("packages/v4-core", { recursive: true });
-  await mkdir("packages/v4-periphery", { recursive: true });
+  await rm("./packages", { recursive: true, force: true });
+  await mkdir("./packages/source", { recursive: true });
 
   const extendedReadme = await readFile("./EXT.md", "utf-8");
 
   for await (const packageName of ["v4-core", "v4-periphery"]) {
-    const packageTarball = await $`npm view @uniswap/${packageName} dist.tarball`;
+    await mkdir(`./packages/${packageName}`, { recursive: true });
 
-    await ky
-      .get(packageTarball.stdout.trim())
-      .then((e) => e.arrayBuffer())
-      .then(async (data) => writeFile(`./packages/source/${packageName}.tgz`, Buffer.from(data)));
+    const [latestSourceManifest, latestRepublishedManifest] = await Promise.all([
+      ky.get(`https://registry.npmjs.org/@uniswap/${packageName}/latest`).json<PackumentVersion>(),
+      ky.get(`https://registry.npmjs.org/${packageName}/latest`).json<PackumentVersion>(),
+    ]);
 
-    await extract({
-      file: `./packages/source/${packageName}.tgz`,
-      cwd: `./packages/${packageName}`,
-    });
+    if (gt(latestSourceManifest.version, latestRepublishedManifest.version)) {
+      await ky
+        .get(latestSourceManifest.dist.tarball)
+        .then((e) => e.arrayBuffer())
+        .then((e) => writeFile(`./packages/source/${packageName}.tgz`, Buffer.from(e)));
 
-    await readFile(`./packages/${packageName}/package/package.json`, "utf-8").then((data) =>
-      writeFile(
-        `./packages/${packageName}/package/package.json`,
-        data
-          .replace(`@uniswap/${packageName}`, packageName) // Remove namespace
-          .replace(
-            `git+https://github.com/Uniswap/${packageName}.git`,
-            `git+https://github.com/hookmate/v4-republishing.git`,
-          ), // Replace repo, keeping homepage
-        "utf-8",
-      ),
-    );
+      await extract({
+        file: `./packages/source/${packageName}.tgz`,
+        cwd: `./packages/${packageName}`,
+      });
 
-    await readFile(`./packages/${packageName}/package/README.md`, "utf-8").then((data) =>
-      writeFile(
-        `./packages/${packageName}/package/README.md`,
-        `${extendedReadme}\n\n${data}`, // Inject README Header
-        "utf-8",
-      ),
-    );
+      await readFile(`./packages/${packageName}/package/package.json`, "utf-8").then((data) =>
+        writeFile(
+          `./packages/${packageName}/package/package.json`,
+          data
+            .replace(`@uniswap/${packageName}`, packageName) // Remove namespace
+            .replace(
+              `git+https://github.com/Uniswap/${packageName}.git`,
+              `git+https://github.com/hookmate/v4-republishing.git`,
+            ), // Replace repo, keeping homepage
+          "utf-8",
+        ),
+      );
 
-    await $`cd ./packages/${packageName}/package && npm publish --access public`.pipe(process.stdout);
+      await readFile(`./packages/${packageName}/package/README.md`, "utf-8").then((data) =>
+        writeFile(
+          `./packages/${packageName}/package/README.md`,
+          `${extendedReadme}\n\n${data}`, // Inject README Header
+          "utf-8",
+        ),
+      );
+
+      await $`cd ./packages/${packageName}/package && npm publish --access public`.pipe(process.stdout);
+    } else {
+      console.log(
+        `Skipping @uniswap/${packageName}, republished version ${latestRepublishedManifest.version} is up-to-date with source version ${latestSourceManifest.version}.`,
+      );
+    }
   }
 }
 
